abb18dc714c2911ab03c70d0850219401372fb85
dmykn <hiherone@gmail.com>
2025-08-10 15:27:20 +0300

    Refactor Firebase Admin initialization to use environment variables

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,6 @@
 // src/pages/home.jsx
 import React, { useEffect, useState } from "react";
-import Navbar from "../../components/Navbar";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   const [banner, setBanner] = useState(null);
