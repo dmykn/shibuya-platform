5c85f43202b615de75e93ceb500401673ea42fca
dmykn <hiherone@gmail.com>
2025-08-10 14:36:12 +0300

    Refactor Firebase Admin initialization to use environment variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 // src/app/page.tsx
-import React from "react";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  if (typeof window !== "undefined") {
-    window.location.href = "/home";
-  }
-  return <div>Redirecting...</div>;
+  redirect("/home");
 }
